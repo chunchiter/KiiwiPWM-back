ae663e0ca3958140b3465f6be2a2491a306d9c4d
Rafael A. Castañon Roman <rafacastajr5@gmail.com>
2025-08-11 21:49:49 -0600

    Integrate ORM

--- src/main/java/com/kiwii/pwm/demo/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kiwii.pwm.demo.repository;
-
-import com.kiwii.pwm.demo.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
