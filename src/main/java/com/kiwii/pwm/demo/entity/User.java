1b83beaf066c7e0211324676fea234cc8c94cb59
Rafael A. Castañon Roman <rafacastajr5@gmail.com>
2025-08-11 21:49:49 -0600

    Integrate ORM

---
+++ src/main/java/com/kiwii/pwm/demo/entity/User.java
@@ -0,0 +1,41 @@
+package com.kiwii.pwm.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // para que el ID se genere automáticamente (autoincrement)
+    private Long id;
+
+    private String username;
+    private String email;
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
